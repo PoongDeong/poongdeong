303205ef522c107fc25dc73afe79c87584fd59eb
woomin <dal96k@gmail.com>
2020-07-16 17:50:25 +0900

    Add postLogin test

--- client/src/apis/auth.test.js
+++ client/src/apis/auth.test.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { postSignUp } from './auth';
+import { postLogin, postSignUp } from './auth';
 
 jest.mock('axios');
 
@@ -14,9 +14,57 @@ describe('auth', () => {
       const id = 'test@example.com';
       const nickname = 'tester';
       const password = '1234';
+
       const token = await postSignUp({ id, nickname, password });
 
       expect(token).toBe('9999');
+    });
+  });
+
+  describe('postLogin', () => {
+    describe('with inexistent id', () => {
+      beforeEach(() => {
+        axios.post.mockResolvedValue({ data: { token: '' } });
+      });
+
+      it('responses token', async () => {
+        const id = 'inexistent@example.com';
+        const password = '1234';
+
+        const token = await postLogin({ id, password });
+
+        expect(token).toBe('');
+      });
+    });
+
+    describe('with correct password', () => {
+      beforeEach(() => {
+        axios.post.mockResolvedValue({ data: { token: '9999' } });
+      });
+
+      it('responses token', async () => {
+        const id = 'test@example.com';
+        const password = '1234';
+
+        const token = await postLogin({ id, password });
+
+        expect(token).toBe('9999');
+      });
+    });
+
+    describe('with incorrect password', () => {
+      beforeEach(() => {
+        axios.post.mockResolvedValue({ data: { token: '' } });
+      });
+
+      it('responses token', async () => {
+        const id = 'test@example.com';
+        const password = '0000';
+
+        const token = await postLogin({ id, password });
+
+        expect(token).toBe('');
+      });
     });
   });
 });
