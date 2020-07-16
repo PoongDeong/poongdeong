7de2112072e68eb224f92b40582ca88939e37b3d
woomin <dal96k@gmail.com>
2020-07-16 17:50:25 +0900

    Add postLogin test

--- client/src/apis/auth.js
+++ client/src/apis/auth.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
+const API_URL = 'https://api.poongdeong.com';
+
 export const postSignUp = async ({ id, nickname, password }) => {
-  const { data } = await axios.post('https://api.poongdeong.com/signup', {
+  const { data } = await axios.post(`${API_URL}/signup`, {
     id, nickname, password,
+  });
+
+  return data.token;
+};
+
+export const postLogin = async ({ id, password }) => {
+  const { data } = await axios.post(API_URL, {
+    id, password,
   });
 
   return data.token;
