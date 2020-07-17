26144ee82f17815eedfadc25166f1732dfeada53
gyim1345 <gyim1345@gmail.com>
2020-07-17 11:02:52 +0900

    Use express validator library for /auth validation
    add validator.js
    delete signup validator.js
    delete login validadtor.js
    
    replace signup and login validator to use express validator library

--- server/src/validator/loginValidator.js
+++
@@ -1,7 +0,0 @@
-export default function loginValidator(req, res, next) {
-  if (!req.body.email || !req.body.password) {
-    return res.status(404).send({ message: 'Empty value' });
-  }
-
-  next();
-}
---
+++ server/src/validator/validator.js
@@ -0,0 +1,14 @@
+import { validationResult } from 'express-validator';
+
+export default function validate(validations) {
+  return async (req, res, next) => {
+    await Promise.all(validations.map((validation) => validation.run(req)));
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    res.status(422).json({ errors: errors.array() });
+  };
+}
