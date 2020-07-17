5ed73aa9d837a5eddb7020615818ef2cbef161e3
younggyulee <baayoo93@naver.com>
2020-07-17 14:50:28 +0900

    Add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import route from './routes';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
 
 app.get('/', (req, res) => {
   res.status(200).send({ message: 'Hello world!' });
