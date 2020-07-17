673b0c452cb1bf3be0d1048624f2107610e1652e
hannut91 <hannut91@gmail.com>
2020-07-17 20:19:56 +0900

    Add socket server
    Add socket server to match.

---
+++ server/src/run.js
@@ -0,0 +1,15 @@
+import server from './io';
+
+import createTable from './hooks/create-table';
+import createUser from './hooks/create-user';
+
+const port = process.env.PORT || 3000;
+
+(async () => {
+  await createTable();
+  await createUser();
+
+  server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+})();
