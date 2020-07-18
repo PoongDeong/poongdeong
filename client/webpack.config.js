103d59aa89b064c370f6b12c561450b1e426825a
woomin <dal96k@gmail.com>
2020-07-18 11:50:39 +0900

    Separate environment server from local

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,41 +1,61 @@
 const path = require('path');
+
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve( __dirname, 'dist/'),
-    filename: 'main.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpeg|png)$/,
-        use: 'file-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: {
-      index: 'index.html',
+const webpack = require('webpack');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = (env, options) => {
+  dotenv.config({
+    path: `env/${options.stage || 'server'}.env`,
+  });
+
+  return {
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+      filename: 'main.js',
     },
-  },
+    node: {
+      fs: 'empty',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpeg|png)$/,
+          use: 'file-loader',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.API_URL': JSON.stringify(process.env.API_URL),
+      }),
+      new webpack.EnvironmentPlugin(['API_URL']),
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: {
+        index: 'index.html',
+      },
+    },
+  };
 };
